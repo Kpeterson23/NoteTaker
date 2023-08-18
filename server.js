1b1a425fe2b0c3680700284a86cde803f12b6765
Kyler <klyerpeterson23@gmail.com>
2023-08-18 16:08:42 -0600

    deleted node modules, trying to get .gitignore to work

--- server.js
+++ server.js
@@ -2,6 +2,7 @@ const express = require("express");
 const path = require("path");
 const api = require("./routes/index.js");
 const fs = require("fs");
+const { v4: uuidv4 } = (require = "uuid");
 
 const PORT = process.env.PORT || 3001;
 
@@ -59,7 +60,7 @@ app.post("/api/notes", (req, res) => {
     const newNote = {
       title,
       text,
-      id: unid(),
+      id: uuidv4(),
     };
 
     readAndAppend(newNote, "./db/db.json");
